c4d4804f4ae7c14d0aa95ad8b39f548989902306
chengzhf <chengzhf_sysu@126.com>
2014-10-30 21:09:48 +0800

    xx
    xx

--- videoEditorDemo/ViewControllers/RecordViewController.m
+++ videoEditorDemo/ViewControllers/RecordViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "RecordViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import <MobileCoreServices/UTCoreTypes.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface RecordViewController ()
-
+- (IBAction)recordAction:(id)sender;
 @end
 
 @implementation RecordViewController
@@ -33,5 +36,59 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)recordAction:(id)sender {
+    [self startCameraControllerFromViewController:self usingDelegate:self];
+}
+
+-(BOOL)startCameraControllerFromViewController:(UIViewController*)controller
+                                 usingDelegate:(id )delegate {
+    // 1 - Validattions
+    if (([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] == NO)
+        || (delegate == nil)
+        || (controller == nil)) {
+        return NO;
+    }
+    // 2 - Get image picker
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+    // Displays a control that allows the user to choose movie capture
+    cameraUI.mediaTypes = [[NSArray alloc] initWithObjects:(NSString *)kUTTypeMovie, nil];
+    // Hides the controls for moving & scaling pictures, or for
+    // trimming movies. To instead show the controls, use YES.
+    cameraUI.allowsEditing = NO;
+    cameraUI.delegate = delegate;
+    // 3 - Display image picker
+    [controller presentViewController: cameraUI animated: YES completion:nil];
+    return YES;
+}
+
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+    [self dismissViewControllerAnimated:NO completion:nil];
+    // Handle a movie capture
+    if (CFStringCompare ((__bridge_retained CFStringRef) mediaType, kUTTypeMovie, 0) == kCFCompareEqualTo) {
+        
+        NSURL *imagePickerURL = [info objectForKey:UIImagePickerControllerMediaURL];
+        NSString *moviePath  = [imagePickerURL path];
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(moviePath)) {
+            UISaveVideoAtPathToSavedPhotosAlbum(moviePath, self,
+                                                @selector(video:didFinishSavingWithError:contextInfo:), nil);
+        }
+    }
+}
+
+-(void)video:(NSString*)videoPath didFinishSavingWithError:(NSError*)error contextInfo:(void*)contextInfo {
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Video Saving Failed"
+                                                       delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video Saved" message:@"Saved To Photo Album"
+                                                       delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
+}
 
 @end
